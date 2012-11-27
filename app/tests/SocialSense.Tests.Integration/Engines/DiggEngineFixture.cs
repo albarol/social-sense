46e13c30026704d907eb8ee3d9ae4ed63e67ba13
Alexandre Oliveira <fakeezz@gmail.com>
2012-11-27 07:55:01 -0200

    create factory

--- app/tests/SocialSense.Tests.Integration/Engines/DiggEngineFixture.cs
+++ app/tests/SocialSense.Tests.Integration/Engines/DiggEngineFixture.cs
@@ -5,7 +5,7 @@
     using SharpTestsEx;
 
     using SocialSense.Engines;
-    using SocialSense.Engines.Configurations;
+    using SocialSense.Extensions;
     using SocialSense.Shared;
 
     [TestFixture, Category("Engines")]
@@ -16,7 +16,7 @@
         [SetUp]
         public void SetUp()
         {
-            this.engine = new Engine(new DiggEngineConfiguration());
+            this.engine = EngineFactory.Digg();
         }
 
         [TestCase("cultura")]
