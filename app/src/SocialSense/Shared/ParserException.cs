43c10882f446cec4f1a0a24d19cbb35590e14960
Alexandre Barbieri <fakeezz@gmail.com>
2013-04-11 14:42:13 -0300

    changed bing parser

---
+++ app/src/SocialSense/Shared/ParserException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SocialSense.Shared
+{
+    public class ParserException : ApplicationException
+    {
+        public ParserException() { }
+        public ParserException(string message) : base(message) { }
+    }
+}
