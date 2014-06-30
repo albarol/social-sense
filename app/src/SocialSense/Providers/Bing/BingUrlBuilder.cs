21c5bfeb04db9ec5a0540af9213ed989104edb99
fakeezz <fakeezz@gmail.com>
2014-06-30 17:51:12 -0300

    update location in bing finder

--- app/src/SocialSense/Providers/Bing/BingUrlBuilder.cs
+++ app/src/SocialSense/Providers/Bing/BingUrlBuilder.cs
@@ -22,35 +22,26 @@ namespace SocialSense.Providers.Bing
                 throw new ArgumentException("term not be null -or- term not be empty");
             }
 
-            this.builder = new StringBuilder("http://www.bing.com/search?");
-            this.builder.AppendFormat("q={0}", query.Term);
+            this.builder = new StringBuilder("https://api.datamarket.azure.com/Bing/Search?format=json");
+            this.builder.AppendFormat("&q={0}", query.Term);
 
-            this.AppendLanguage(query.Language);
             this.AppendCountry(query.Country);
             this.AppendPage(query.Page);
 
             return this.builder.ToString();
         }
 
-        private void AppendLanguage(Language? language)
-        {
-            if (language.HasValue && language.Value != Language.Undefined)
-            {
-                this.builder.AppendFormat(" +language:{0}", this.location.GetLanguage(language.Value));
-            }
-        }
-
         private void AppendCountry(Country? country)
         {
             if (country.HasValue && country.Value != Country.Undefined)
             {
-                this.builder.AppendFormat(" +loc:{0}", this.location.GetCountry(country.Value));
+                this.builder.AppendFormat("{0}", this.location.GetCountry(country.Value));
             }
         }
 
         private void AppendPage(int page)
         {
-            this.builder.AppendFormat("&first={0}", ((page * 10) - 10) + 1);
+            this.builder.AppendFormat("&skip={0}", (page * 50) - 50);
         }
     }
 }
